5434336566e078e1390a4b8154f36bb51588bb35
nhithaivn <nhithai@orgos.net>
2021-06-04 10:50:12 +0700

    update document file

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,14 +11,26 @@ class MyDocument extends Document {
       <Html lang="en">
 
         <Head>
-          <meta name="description" content="Build a responsive PWA using Next.js and styled-components."></meta>
-          <meta name="theme-color" content="#FFFFFF"/>
-          
           <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+          <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/manifest.json" />
-          <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
-          <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta name="description" content="Build a responsive PWA using Next.js and styled-components."></meta>
+         
           <link
           href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.min.css"
           rel="stylesheet"
@@ -27,8 +39,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/js/swiper.js"></script>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/js/swiper.min.js"></script>  
+          {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/js/swiper.min.js"></script>   */}
         </body>
       </Html>
     );
